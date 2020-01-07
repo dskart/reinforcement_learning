74c7132b91d0c883bd86d62e50b31962eb4d3ff5
Raphael Van Hofffelen <40248256+dskart@users.noreply.github.com>
2020-01-07 10:54:51 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# reinforcement_learning
-implementation of a Q-learning agent from scratch with epsilon-greedy actions on different environment (2D grid world and pacman game)
+# Reinforcement Learning
+
+This repository contains the code for a Q-learning agent, which does very little on construction, but instead learns by trial and error from interactions with the environment through its update method. The agent aslo has access to epsilon-greedy actions. This agent is tested on a 2D grid world as well a pacman simulation.
+This implementation was written without the use of any external library apart from Numpy.
+
+This was made as an exercise to implement a reinforcement learning agent from scratch and run it on several different games. So please take in account that this code was written in a few days without any professional review/standard.
+
+## Getting Started
+
+This repository contains a lot of files. The only interesting file is ["qlearningAgents.py"](qlearningAgents.py) where the code for the Q learning agent is located.
+
+### Prerequisites
+
+- [Numpy](https://numpy.org/)
+
+## Running the simulations
+
+### Grid world
+
+(with epsilon greedy random actions)
+
+```[Python]
+python3 gridworld.py -a q -k 100
+```
+
+### Bridge crossing
+
+``` [Python]
+python3 gridworld.py -a q -k 50 -n 0 -g BridgeGrid -e 1
+```
+
+Epsilon is controlled by -e, learning rate by -l.
+
+### Pacman
+
+```[python]
+python3 pacman.py -p PacmanQAgent -x 2000 -n 2010 -l smallGrid
+```
+
+### Pacman with approximate Q-Learning
+
+This works on bigger pacman grids compare to the classical Q-Learning agent
+
+```[pyton]
+python3 pacman.py -p ApproximateQAgent -a extractor=SimpleExtractor -x 50 -n 60 -l mediumClassic
+```
+
+## Authors
+
+- **Raphael Van Hoffelen** - [github](https://github.com/dskart) - [website](https://www.raphaelvanhoffelen.com/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ qlearningAgents.py
@@ -0,0 +1,231 @@
+# qlearningAgents.py
+# ------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+from game import *
+from learningAgents import ReinforcementAgent
+from featureExtractors import *
+import numpy as np
+
+
+import random
+import util
+import math
+
+
+class QLearningAgent(ReinforcementAgent):
+    """
+      Q-Learning Agent
+
+      Functions you should fill in:
+        - computeValueFromQValues
+        - computeActionFromQValues
+        - getQValue
+        - getAction
+        - update
+
+      Instance variables you have access to
+        - self.epsilon (exploration prob)
+        - self.alpha (learning rate)
+        - self.discount (discount rate)
+
+      Functions you should use
+        - self.getLegalActions(state)
+          which returns legal actions for a state
+    """
+
+    def __init__(self, **args):
+        "You can initialize Q-values here..."
+        ReinforcementAgent.__init__(self, **args)
+        self._q_values = {}
+
+    def getQValue(self, state, action):
+        """
+          Returns Q(state,action)
+          Should return 0.0 if we have never seen a state
+          or the Q node value otherwise
+        """
+        if (state, action) in self._q_values:
+            return self._q_values[(state, action)]
+        else:
+            return 0.0
+
+    def setQValue(self, state, action, value):
+        self._q_values[(state, action)] = value
+
+    def computeValueFromQValues(self, state):
+        """
+          Returns max_action Q(state,action)
+          where the max is over legal actions.  Note that if
+          there are no legal actions, which is the case at the
+          terminal state, you should return a value of 0.0.
+        """
+        q_values = [self.getQValue(state, action)
+                    for action in self.getLegalActions(state)]
+        if not len(q_values):
+            return 0.0
+
+        return max(q_values)
+
+    def computeActionFromQValues(self, state):
+        """
+          Compute the best action to take in a state.  Note that if there
+          are no legal actions, which is the case at the terminal state,
+          you should return None.
+        """
+        value = self.getValue(state)
+
+        optimal_actions = []
+        for action in self.getLegalActions(state):
+            if self.getQValue(state, action) == value:
+                optimal_actions.append(action)
+
+        if not len(optimal_actions):
+            return None
+
+        return random.choice(optimal_actions)
+
+    def getAction(self, state):
+        """
+          Compute the action to take in the current state.  With
+          probability self.epsilon, we should take a random action and
+          take the best policy action otherwise.  Note that if there are
+          no legal actions, which is the case at the terminal state, you
+          should choose None as the action.
+
+          HINT: You might want to use util.flipCoin(prob)
+          HINT: To pick randomly from a list, use random.choice(list)
+        """
+        actions = self.getLegalActions(state)
+
+        # flip coing with proba self.epsilon
+        if util.flipCoin(self.epsilon):
+            action = random.choice(actions)
+        else:
+            action = self.getPolicy(state)
+
+        return action
+
+    def update(self, state, action, nextState, reward):
+        """
+          The parent class calls this to observe a
+          state = action => nextState and reward transition.
+          You should do your Q-Value update here
+          NOTE: You should never call this function,
+          it will be called on your behalf
+        """
+        discount = self.discount
+        alpha = self.alpha
+        q_value = self.getQValue(state, action)
+        v = self.getValue(nextState)
+
+        new_v = (1-alpha) * q_value + alpha * (reward + discount * v)
+
+        self.setQValue(state, action, new_v)
+
+        return
+
+    def getPolicy(self, state):
+        return self.computeActionFromQValues(state)
+
+    def getValue(self, state):
+        return self.computeValueFromQValues(state)
+
+
+class PacmanQAgent(QLearningAgent):
+    "Exactly the same as QLearningAgent, but with different default parameters"
+
+    def __init__(self, epsilon=0.05, gamma=0.8, alpha=0.2, numTraining=0, **args):
+        """
+        These default parameters can be changed from the pacman.py command line.
+        For example, to change the exploration rate, try:
+            python pacman.py -p PacmanQLearningAgent -a epsilon=0.1
+        alpha    - learning rate
+        epsilon  - exploration rate
+        gamma    - discount factor
+        numTraining - number of training episodes, i.e. no learning after these many episodes
+        """
+        args['epsilon'] = epsilon
+        args['gamma'] = gamma
+        args['alpha'] = alpha
+        args['numTraining'] = numTraining
+        self.index = 0  # This is always Pacman
+        QLearningAgent.__init__(self, **args)
+
+    def getAction(self, state):
+        """
+        Simply calls the getAction method of QLearningAgent and then
+        informs parent of action for Pacman.  Do not change or remove this
+        method.
+        """
+        action = QLearningAgent.getAction(self, state)
+        self.doAction(state, action)
+        return action
+
+
+class ApproximateQAgent(PacmanQAgent):
+    """
+       ApproximateQLearningAgent
+       You should only have to overwrite getQValue
+       and update.  All other QLearningAgent functions
+       should work as is.
+    """
+
+    def __init__(self, extractor='IdentityExtractor', **args):
+        self.featExtractor = util.lookup(extractor, globals())()
+        PacmanQAgent.__init__(self, **args)
+
+        # You might want to initialize weights here.
+
+        self.weights = util.Counter()
+
+    def getWeights(self):
+        return self.weights
+
+    def getQValue(self, state, action):
+        """
+          Should return Q(state,action) = w * featureVector
+          where * is the dotProduct operator
+        """
+        features = self.featExtractor.getFeatures(state, action)
+
+        value = 0
+        for key in features:
+            value += self.weights[key] * features[key]
+
+        return value
+
+    def update(self, state, action, nextState, reward):
+        """
+           Should update your weights based on transition
+        """
+
+        features = self.featExtractor.getFeatures(state, action)
+
+        diff = (reward + self.discount *
+                self.getValue(nextState)) - self.getQValue(state, action)
+
+        for feature in features:
+            self.weights[feature] += self.alpha * diff * features[feature]
+
+    def final(self, state):
+        "Called at the end of each game."
+        # call the super-class final method
+        PacmanQAgent.final(self, state)
+
+        # did we finish training?
+        if self.episodesSoFar == self.numTraining:
+            # you might want to print your weights here for debugging
+            "*** YOUR CODE HERE ***"
+            print(self.weights)
+            pass
